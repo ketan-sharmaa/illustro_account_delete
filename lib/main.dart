a90edfb75435607620c9fbff7249de4fe468e63e
Ketan's Macbook Pro <ketan.sharma@softedge.in>
2024-09-08 15:48:40 +0530

    work

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
-          
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text('Enter email for account delete'),
+              TextField(
+                controller: controller,
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    try {
+                      final ref = await FirebaseFirestore.instance
+                          .collection('deleted_accounts');
+                      ref.doc(controller.text).set({"delete": true});
+                      // ignore: use_build_context_synchronously
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text(
+                              "Submitted, Account will be deleted in 7 days.")));
+                    } catch (e) {
+                      // ignore: use_build_context_synchronously
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text("Somethin went wrong")));
+                    }
+                  },
+                  child: const Text('Submit'))
+            ],
+          ),
         ),
       ),
     );
